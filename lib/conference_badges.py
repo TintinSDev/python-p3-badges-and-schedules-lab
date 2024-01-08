91d1182860bc36fdd358b2116f39b358940e4f26
Martin <mmaina290@gmail.com>
2024-01-08 23:51:17 +0300

    for loops

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -4,11 +4,6 @@ def badge_maker(name):
 
 def batch_badge_creator(names):
     return [badge_maker(name) for name in names]
-# speaker_names = ["Arel", "Carol", "Certo"]
-# badges = batch_badge_creator(speaker_names)
-
-# for badge in badges:
-#     print(badge)
 
 def assign_rooms(names):
      room_assignments = []
@@ -18,11 +13,6 @@ def assign_rooms(names):
     
      return room_assignments
 
-# names = ["Arel", "Carol", "John"]
-# room_assignments = assign_rooms(names)
-
-# for assignment in room_assignments:
-#     print(assignment)
 
 def printer(names):
     badges = batch_badge_creator(names)
